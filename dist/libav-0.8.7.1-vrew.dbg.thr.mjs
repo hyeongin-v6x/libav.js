8d1d48bc1d9afd6527ac169ee7a6c4ab6cdc8330
Hyeongin Park <hyeongin.park@voyagerx.com>
2025-08-04 12:19:55 +0900

    test mov.c modification

--- dist/libav-0.8.7.1-vrew.dbg.thr.mjs
+++ dist/libav-0.8.7.1-vrew.dbg.thr.mjs
@@ -1442,7 +1442,7 @@ var tempI64;
 
 // === Body ===
 var ASM_CONSTS = {
-  771545: () => {
+  771577: () => {
     Fibers.trampolineRunning = false;
   }
 };
@@ -7337,7 +7337,7 @@ var _asyncify_start_rewind = a0 => (_asyncify_start_rewind = wasmExports["asynci
 
 var _asyncify_stop_rewind = () => (_asyncify_stop_rewind = wasmExports["asyncify_stop_rewind"])();
 
-var _ff_h264_cabac_tables = Module["_ff_h264_cabac_tables"] = 545356;
+var _ff_h264_cabac_tables = Module["_ff_h264_cabac_tables"] = 545388;
 
 
 // === Auto-generated postamble setup entry stuff ===
